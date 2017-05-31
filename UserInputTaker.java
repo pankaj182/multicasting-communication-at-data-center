a019d7f13bd1e47bdb01340a6c163642725c1ab4
Pankaj Kumar <pankajkumar1802@gmail.com>
2017-05-31 12:23:22 +0530

    This class will take input from user

---
+++ UserInputTaker.java
@@ -0,0 +1,11 @@
+package iitkgp;
+/**
+ * @since  31/05/2017 Wednesday
+ * @author pankaj
+ * @location Indian Institute of Technology, Kharagpur
+ * <h3>Description</h3>
+ * This class takes input from user as required by the program.<p>
+ */
+public class UserInputTaker {
+	
+}
