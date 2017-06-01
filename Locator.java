56d4465769806c3fd3c9cbfd267cca47eb2e9a6f
Pankaj Kumar <pankajkumar1802@gmail.com>
2017-06-01 15:42:42 +0530

    to locate nearby nodes

---
+++ Locator.java
@@ -0,0 +1,11 @@
+package iitkgp;
+
+public abstract class Locator {
+	public static int nearestSwitch(int x,int y){
+		
+		return Integer.MAX_VALUE;
+	}
+	public static int nearestCore(int x,int y){
+		return Integer.MAX_VALUE;
+	}
+}
