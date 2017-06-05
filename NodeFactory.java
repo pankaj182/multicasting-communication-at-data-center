a55a193178eaad3fcf0f5900dae2ef01619a626f
Pankaj Kumar <pankajkumar1802@gmail.com>
2017-06-05 22:23:14 +0530

    User class modified, hence object returning method needed to be modified

--- NodeFactory.java
+++ NodeFactory.java
@@ -1,12 +1,44 @@
 package iitkgp;
-
+/**
+ * @since 02/06/2017
+ * @author pankaj
+ * @location :Indian Institute of Technology,Kharagpur
+ * <p>this abstract class is a factory class used to return instances of a class.
+ * @see Switch
+ * @see Core
+ * @see User
+ * @see NodeFactory#getUser(int, int, int)
+ * @see NodeFactory#getSwitch(int)
+ * @see NodeFactory#getCore(int)
+ */
 public abstract class NodeFactory {
+	/**
+	 * This method return instance of a User class. The parameters defines the characterstics of a user.
+	 * @param id The identity given to a user.The location is its ID= 100*x+y
+	 * @param u The uploading capacity of a user
+	 * @param d the downloading capacity of the user.
+	 * @return instance of User class
+	 * @see User
+	 */
 	public static User getUser(int id,int u,int d){
-		return new User(id,u,d);
+		return new User(id,u,d,Database.getEnergy());
 	}
+	/**
+	 * This method is used to return instance of Switch class
+	 * @param id The identity given to a switch.The location is its ID= 100*x+y
+	 * @return instance of Switch class.
+	 * @see Switch
+	 */
 	public static Switch getSwitch(int id){
 		return new Switch(id);
 	}
+	
+	/**
+	 * This method is used to return instance of Core class
+	 * @param id The identity given to a core.The location is its ID= 100*x+y
+	 * @return instance of Core class.
+	 * @see Core
+	 */
 	public static Core getCore(int id){
 		return  new Core(id);
 	}
